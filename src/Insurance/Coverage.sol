72f05af9c72cb45f98d07723856713ac3ba9a086
Evans007 <ebounce500@gmail.com>
2023-04-28 10:20:43 +0100

    working on coverage

--- src/Insurance/Coverage.sol
+++ src/Insurance/Coverage.sol
@@ -2,13 +2,15 @@
 
 pragma solidity ^0.8.13;
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
 
-contract Cover{
+contract Cover is AccessControl, Ownable{
 uint256 insureId;
 uint256 private constant MINIMUM_POLICY_DURATION = 1 weeks; // minimum period in which insurance covers
 uint256 private constant MAXIMUM_POLICY_DURATION = 365 days; // maximum period in which insurance covers
-
+bytes32 public constant ADMIN_ROLE = keccak256("NEO_ADMIN");
+bytes32 public constant MAJOR_ADMIN= keccak256("MAJORADMIN");
 // Struct to create Insurance Policy
 
     struct InsurancePolicy{
@@ -30,30 +32,47 @@ uint256 private constant MAXIMUM_POLICY_DURATION = 365 days; // maximum period i
     }
 
     mapping(uint => InsurancePolicy) public insurePolicy;
-    InsurancePolicy[] public arrayPolicy;
     mapping (address => mapping( uint => PolicyPurchase)) public  policyBought;
 
+    InsurancePolicy[] public arrayPolicy;
+    address[] public admins;
 
+    constructor() {
+        Admin = msg.sender;
+    }
 
 // Register Admin
+    function registerAdmin(address _newAdmin) external onlyOwner {
+        if(_newAdmin == address(0x0)) revert("Address zero detected");
+        _setupRole(ADMIN_ROLE, _newAdmin);
+        admins.push(_newAdmin);
+    }
+
+    function showAdmins() external view returns (address[] memory) {
+        return admins;
+    }
+
+    function verifyAdmin(address _admin) internal view returns(bool){
+        return hasRole(ADMIN_ROLE, _admin);
+    } 
 
 // create an insurance policy
 // Only chosen Admin can create Insurance Policy
-function createInsurancePolicy(string memory _policyName, string [] memory _agreement, string[] memory _policyOffer, uint _minimumPeriod, uint _maximumPeriod) external{
-        insureId +=1;
+function createInsurancePolicy(string memory _policyName, string [] memory _agreement, string[] memory _policyOffer, uint _minimumPeriod, uint _maximumPeriod) external onlyRole(ADMIN_ROLE){
+    insureId +=1;
 
-InsurancePolicy storage createPolicy = insurePolicy[insureId];
-createPolicy.PolicyName = _policyName;
-createPolicy.PolicyActive = true;
-createPolicy.Agreement = abi.encode(_agreement);
-createPolicy.PolicyOffer = abi.encode(_policyOffer);
-createPolicy.MinimumPeriod = _minimumPeriod;
-createPolicy.MaximumPeriod = _maximumPeriod;
+    InsurancePolicy storage createPolicy = insurePolicy[insureId];
+    createPolicy.PolicyName = _policyName;
+    createPolicy.PolicyActive = true;
+    createPolicy.Agreement = abi.encode(_agreement);
+    createPolicy.PolicyOffer = abi.encode(_policyOffer);
+    createPolicy.MinimumPeriod = _minimumPeriod;
+    createPolicy.MaximumPeriod = _maximumPeriod;
 
-InsurancePolicy memory newPolicy = InsurancePolicy({PolicyName:_policyName, PolicyActive:true, Agreement:abi.encode(_agreement), PolicyOffer: abi.encode(_policyOffer), MinimumPeriod: _minimumPeriod, MaximumPeriod: _maximumPeriod });
-arrayPolicy.push(newPolicy);
+    InsurancePolicy memory newPolicy = InsurancePolicy({PolicyName:_policyName, PolicyActive:true, Agreement:abi.encode(_agreement), PolicyOffer: abi.encode(_policyOffer), MinimumPeriod: _minimumPeriod, MaximumPeriod: _maximumPeriod });
+    arrayPolicy.push(newPolicy);
 
-    }
+}
 
 
 // buy Automobile policy
