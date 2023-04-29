4102db09ef8fb7b327dcaa90af227c4155c37033
milk-maid <roqbell@gmail.com>
2023-04-29 01:32:02 +0100

    resolving

---
+++ src/roqbell_moto/auto.sol
@@ -0,0 +1,9 @@
+// SPDX-License_Identifier: MIT
+
+pragma solidity ^0.8.13;
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract Auto {
+    
+}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "/";
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
